7a534520af41ab70c12b4d46547d8073c9294327
bscren <mayuxuan61@gmail.com>
2026-01-07 21:43:42 +0800

    third_save

---
+++ Practice_Bag/practice.cpp
@@ -0,0 +1,11 @@
+// 一个进行C++练习的文件,主要用于测试多进程计算随机数的开方相关的功能
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, Practice Bag!" << endl;
+
+    // TODO: 添加多进程计算随机数开方的代码
+    
+    return 0;
+}
